361f1ff1c57bb0305883a278947a5c243d4cbb16
FleterEwenn <fleter.ewenn@gmail.com>
2026-01-10 18:25:53 +0100

    creation des conditions if

--- minilangage.py
+++ minilangage.py
@@ -1,6 +1,7 @@
 from tkinter import *
 
 var = {}
+list_if_codes = []
 
 def replace_by_values(string:str)->str:
     for name in var.keys():
@@ -18,9 +19,25 @@ def split_code():
     code = zone_text.get('1.0', END).strip()
     lines = code.split("\n")
     clean_lines = []
+
+    if_code = []
+    on_if = False
+
     for line in lines:
-        if line != "":
+
+        if on_if:
+            if_code.append(line.strip())
+        
+        if line != "" and not on_if:
             clean_lines.append(line.strip())
+
+        if line != "" and line.strip()[-1] == "{":
+            on_if = True
+        if line != "" and line.strip()[-1] == "}":
+            on_if = False
+            list_if_codes.append(if_code)
+            if_code = []
+
     executer_code(clean_lines)
 
 def ecrire_console(text_to_print):
@@ -31,6 +48,7 @@ def ecrire_console(text_to_print):
 def executer_code(code:list[str]):
 
     for line in code:
+        print(line)
         if line.startswith("spinjutzu "): # affichage dans la console
             try:
                 current_line = line
@@ -65,12 +83,13 @@ def executer_code(code:list[str]):
             
             except NameError as e:
                 name = ""
-                on_name = True
+                on_name = False
                 for char in str(e):
                     if on_name:
                         name += char
                     if char == "'":
                         on_name = not on_name
+                ecrire_console(f"la variable '{name[:-1]}' n'est pas definie, ligne {list_line.index(line) + 1}")
             except Exception as e:
                 ecrire_console(e)
 
@@ -109,17 +128,55 @@ def executer_code(code:list[str]):
 
             except NameError as e:
                 name = ""
-                on_name = True
+                print(e)
+                on_name = False
                 for char in str(e):
                     if on_name:
                         name += char
                     if char == "'":
                         on_name = not on_name
 
-                ecrire_console(f"la variable '{name[-1]}' n'est pas definie")
+                ecrire_console(f"la variable '{name[:-1]}' n'est pas definie, ligne {code.index(line) + 1}")
             except Exception as e:
                 ecrire_console(e)
-    
+
+        elif line.startswith("wu "): # if
+            list_line = line.split("{")
+            bool_ = list_line[0][2:-1]
+
+            list_text = bool_.split("'")
+            if len(list_text)%2 == 0:
+                raise SyntaxError(f"syntaxe invalide, ligne {code.index(line) + 1} : la chaine de caractère est mal delimitée")
+
+            list_string = []
+
+            on_string = False
+            string = ""
+            for char in bool_:
+                if on_string:
+                    string += char
+                if char == "'":
+                    if on_string:
+                        list_string.append(string[:-1])
+                    string = ""
+                    on_string = not on_string
+                
+            for i in range(len(list_text)):
+                if list_text[i] in list_string:
+                    list_text[i] = "'" + list_text[i] + "'"
+                else:
+                    list_text[i] = replace_by_values(list_text[i])
+                
+            bool_ = "".join(list_text)
+            bool_ = eval(bool_)
+
+            if bool_:
+                executer_code(list_if_codes[0])
+                list_if_codes.pop(0)
+
+        elif line != "}":
+            ecrire_console(f"la commande n'existe pas, ligne {code.index(line) + 1}")
+        
         print(var)
 
 window = Tk()
